e4f59de3f769c36c97b88a6a107a9064bb3503ca
unterrrkunft <114381125+unterrrkunft@users.noreply.github.com>
2026-04-13 07:46:11 +0300

    Add files via upload

---
+++ lab7/src/components/TaskList.jsx
@@ -0,0 +1,40 @@
+import { useContext, useRef, useState } from 'react'
+import { TaskContext } from '../context/TaskContext'
+import TaskItem from './TaskItem'
+
+export default function TaskList() {
+  const { filteredTasks, addTask } = useContext(TaskContext)
+
+  const [text, setText] = useState('')
+  const inputRef = useRef(null)
+
+  const handleAdd = () => {
+    if (!text.trim()) return
+
+    addTask(text)
+    setText('')
+
+    // 🔹 фокус назад в інпут
+    inputRef.current?.focus()
+  }
+
+  return (
+    <div>
+      <div className="input-box">
+        <input
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="New task..."
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+
+      <ul>
+        {filteredTasks.map(task => (
+          <TaskItem key={task.id} task={task} />
+        ))}
+      </ul>
+    </div>
+  )
+}
